3fad02bc504ac1ddc0c22ebf99b72cfe837576e9
Martin Vidner <mvidner@suse.cz>
2016-05-27 17:33:52 +0200

    Put into classes
    http://www.sinatrarb.com/intro.html#Sinatra::Base%20-%20Middleware,%20Libraries,%20and%20Modular%20Apps

--- bar.rb
+++ bar.rb
@@ -1,15 +1,19 @@
-require "sinatra"
+require "sinatra/base"
 require "haml"
 
-get "/" do
-  haml '%a{:href => url("/triple")} Triple'
-end
+class BarApp < Sinatra::Base
+  get "/" do
+    haml '%a{:href => url("/triple")} Triple'
+  end
 
-get "/triple" do
-  "Here we triple numbers"
-end
+  get "/triple" do
+    "Here we triple numbers"
+  end
 
+  get "/triple/:value" do
+    (params["value"] * 3).to_s
+  end
 
-get "/triple/:value" do
-  (params["value"] * 3).to_s
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
--- foo.rb
+++ foo.rb
@@ -1,15 +1,19 @@
-require "sinatra"
+require "sinatra/base"
 require "haml"
 
-get "/" do
-  haml '%a{:href => url("/double")} Double'
-end
+class FooApp < Sinatra::Base
+  get "/" do
+    haml '%a{:href => url("/double")} Double'
+  end
 
-get "/double" do
-  "Here we double numbers"
-end
+  get "/double" do
+    "Here we double numbers"
+  end
 
+  get "/double/:value" do
+    (params["value"] * 2).to_s
+  end
 
-get "/double/:value" do
-  (params["value"] * 2).to_s
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
